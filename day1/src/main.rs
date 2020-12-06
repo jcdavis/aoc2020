a871135f80719108df05ab969590d24a899f7358
Jackson Davis <jackson@jcdav.is>
2020-12-05 19:53:04 -0800

    day1p1

---
+++ day1/src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::fs;
+
+use std::collections::HashSet;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input = fs::read_to_string(&args[1]).unwrap();
+    let nums: HashSet<i32> = input.lines().map(|line| line.parse::<i32>().unwrap()).collect();
+
+    for num in &nums {
+        if nums.contains(&(2020-num)) {
+            println!("{}", num*(2020-num));
+        }
+    }
+}
