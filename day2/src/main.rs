41fd0a9ae566ad489e1e60523ae3b33dc992868d
Jackson Davis <jackson@jcdav.is>
2020-12-05 20:45:55 -0800

    d2p2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,23 +8,19 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let input = fs::read_to_string(&args[1]).unwrap();
     let re = Regex::new(r"(\d+)-(\d+) (\w): (.*)").unwrap();
-    let lines: Vec<(i32, i32, char, String)> = input.lines().map(|line| {
+    let lines: Vec<(usize, usize, char, String)> = input.lines().map(|line| {
         let mat = re.captures(line).unwrap();
-        (mat[1].parse().unwrap(), mat[2].parse().unwrap(), mat[3].chars().next().unwrap(), mat[4].to_string())
+        (mat[1].parse::<usize>().unwrap(), mat[2].parse::<usize>().unwrap(), mat[3].chars().next().unwrap(), mat[4].to_string())
     }).collect();
 
     let mut matching = 0;
 
     for line in &lines {
         let (min, max, target, str) = line;
-        let mut count  = 0;
-        str.chars().for_each(|ch| {
-            if  &ch == target {
-                count+= 1;
-            }
-        });
-        if &count >= min && &count <= max {
-            matching += 1;
+        let chars: Vec<char> = str.chars().collect();
+
+        if (chars.get(*min - 1) == Some(target)) ^ (chars.get(*max - 1) == Some(target)) {
+            matching += 1
         }
     }
 
