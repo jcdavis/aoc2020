806df53026d8f45ec0e1e7d4012ba476181217b0
Jackson Davis <jackson@jcdav.is>
2020-12-05 19:54:59 -0800

    p2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,9 +8,12 @@ fn main() {
     let input = fs::read_to_string(&args[1]).unwrap();
     let nums: HashSet<i32> = input.lines().map(|line| line.parse::<i32>().unwrap()).collect();
 
-    for num in &nums {
-        if nums.contains(&(2020-num)) {
-            println!("{}", num*(2020-num));
+    for num1 in &nums {
+        for num2 in &nums {
+            let rem = 2020-num1-num2;
+            if nums.contains(&rem) {
+                println!("{}", num1*num2*rem);
+            }
         }
     }
 }
