58b12e40b4490301195c2b372d06de0702d0cca9
Jackson Davis <jackson@jcdav.is>
2020-12-06 17:13:56 -0800

    d3p2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,16 +6,23 @@ fn main() {
     let input = fs::read_to_string(&args[1]).unwrap();
 
     let chars: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+
+    let count = count(&chars, 1, 1) * count(&chars, 3, 1) *
+        count(&chars, 5, 1) * count(&chars, 7, 1) * count(&chars, 1, 2);
+    println!("{}", count);
+}
+
+fn count(chars: &Vec<Vec<char>>, xd: usize, yd: usize) -> i64 {
     let mut x: usize = 0;
     let mut y: usize = 0;
-    let mut count = 0;
+    let mut count: i64 = 0;
 
     while y < chars.len() {
         if chars[y][x] == '#' {
             count += 1;
         }
-        y += 1;
-        x = (x+3) % chars[0].len();
+        y += yd;
+        x = (x + xd) % chars[0].len();
     }
-    println!("{}", count);
+    count
 }
