80fd4731ab26df3c4e3bf2a31495886a3bb38edf
Jackson Davis <jackson@jcdav.is>
2020-12-06 21:10:07 -0800

    d5p2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -22,15 +22,17 @@ fn to_seat(s: &str) -> (i32, i32) {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input = fs::read_to_string(&args[1]).unwrap();
-    let mut max = 0;
 
-    for line in input.lines().into_iter() {
+    let mut vec: Vec<i32> = input.lines().map(|line| {
         let (row, col) = to_seat(line);
-        let id = row*8 + col;
-        if id > max {
-            max = id;
+        row*8 + col
+    }).collect();
+
+    vec.sort();
+
+    for i in 1..vec.len() {
+        if vec[i] - vec[i-1] == 2 {
+            println!("{}", vec[i]-1);
         }
     }
-
-    println!("{}", max);
 }
