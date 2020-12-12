ee97955630fe42f8521f6b3778e7f386659bc2de
Jackson Davis <jackson@jcdav.is>
2020-12-12 13:20:36 -0800

    d8p2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -5,24 +5,18 @@ use std::fs;
 use std::collections::HashSet;
 
 use regex::Regex;
+use std::process::exit;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let input = fs::read_to_string(&args[1]).unwrap();
-    let re = Regex::new(r"(\w+) ([+-]\d+)").unwrap();
-
-    let instructions: Vec<(String, i32)> = input.lines().map(|line| {
-        let c = re.captures(line).unwrap();
-        let val = c[2].parse::<i32>().unwrap();
-        (c[1].to_string(), val)
-    }).collect();
-
+fn run_program(instructions: &Vec<(String, i32)>) -> Option<i32> {
     let mut visited: HashSet<usize> = HashSet::new();
     let mut current: usize = 0;
     let mut acc = 0;
     loop {
+        if current == instructions.len() {
+            return Some(acc);
+        }
         if visited.contains(&current) {
-            break;
+            return None;
         }
         visited.insert(current);
         let num = instructions[current].1;
@@ -42,5 +36,39 @@ fn main() {
             }
         }
     }
-    println!("{}", acc);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input = fs::read_to_string(&args[1]).unwrap();
+    let re = Regex::new(r"(\w+) ([+-]\d+)").unwrap();
+
+    let mut instructions: Vec<(String, i32)> = input.lines().map(|line| {
+        let c = re.captures(line).unwrap();
+        let val = c[2].parse::<i32>().unwrap();
+        (c[1].to_string(), val)
+    }).collect();
+
+    for i in 0..instructions.len() {
+        let num = instructions[i].1;
+        match instructions[i].0.as_str() {
+            "nop" => {
+                instructions[i] = ("jmp".to_string(), num);
+                run_program(&instructions).map(|res| {
+                    println!("{}", res);
+                    exit(0);
+                });
+                instructions[i] = ("nop".to_string(), num);
+            }
+            "jmp" => {
+                instructions[i] = ("nop".to_string(), num);
+                run_program(&instructions).map(|res| {
+                    println!("{}", res);
+                    exit(0);
+                });
+                instructions[i] = ("jmp".to_string(), num);
+            }
+            _ => { }
+        }
+    }
 }
