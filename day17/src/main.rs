3131079b7c549b2607b3ad89150a7cb8fe2328ee
Jackson Davis <jackson@jcdav.is>
2020-12-19 18:27:47 -0800

    d17p2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -3,25 +3,27 @@ use std::fs;
 use std::mem;
 use std::collections::{HashSet, HashMap};
 
-fn insert_adjencies(pos: &[i32;3], set: &mut HashSet<[i32;3]>, include_self: bool) {
+fn insert_adjencies(pos: &[i32;4], set: &mut HashSet<[i32;4]>, include_self: bool) {
     for i in -1..=1 {
         for j in -1..=1 {
             for k in -1..=1 {
-                if i != 0 || j != 0 || k != 0 || include_self {
-                    set.insert([pos[0]+i, pos[1] + j, pos[2] + k]);
+                for w in -1..=1 {
+                    if i != 0 || j != 0 || k != 0 || w != 0 || include_self {
+                        set.insert([pos[0] + i, pos[1] + j, pos[2] + k, pos[3] + w]);
+                    }
                 }
             }
         }
     }
 }
-fn gen_adjencies(pos: &[i32;3], include_self: bool) -> HashSet<[i32;3]> {
-    let mut res: HashSet<[i32;3]> = HashSet::new();
+fn gen_adjencies(pos: &[i32;4], include_self: bool) -> HashSet<[i32;4]> {
+    let mut res: HashSet<[i32;4]> = HashSet::new();
     insert_adjencies(pos, &mut res, include_self);
     res
 }
 
-fn next_turn(current: &HashMap<[i32;3], bool>, next: &mut HashMap<[i32;3], bool>) {
-    let mut candidates: HashSet<[i32;3]> = HashSet::new();
+fn next_turn(current: &HashMap<[i32;4], bool>, next: &mut HashMap<[i32;4], bool>) {
+    let mut candidates: HashSet<[i32;4]> = HashSet::new();
     for (square, _) in current {
         insert_adjencies(square, &mut candidates, true);
     }
@@ -42,15 +44,15 @@ fn next_turn(current: &HashMap<[i32;3], bool>, next: &mut HashMap<[i32;3], bool>
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input = fs::read_to_string(&args[1]).unwrap();
-    let mut current: HashMap<[i32;3], bool> = HashMap::new();
-    let mut next: HashMap<[i32;3], bool> = HashMap::new();
+    let mut current: HashMap<[i32;4], bool> = HashMap::new();
+    let mut next: HashMap<[i32;4], bool> = HashMap::new();
 
     let mut i = 0;
     for line in input.lines() {
         let mut j = 0;
         for c in line.chars() {
             if c == '#' {
-                current.insert([i, j, 0], true);
+                current.insert([i, j, 0, 0], true);
             }
             j += 1;
         }
