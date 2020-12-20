0210e3b50904d46750617524318996473257d7ba
Jackson Davis <jackson@jcdav.is>
2020-12-19 17:31:15 -0800

    d16p2

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -2,7 +2,7 @@ extern crate regex;
 
 use std::env;
 use std::fs;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use regex::Regex;
 use std::borrow::Borrow;
@@ -38,31 +38,70 @@ fn main() {
     }
     // skip empty, "your ticket:"
     i += 2;
-    let _my_ticket: Vec<i32> = lines[i].split(',').map(|n| n.parse().unwrap()).collect();
+    let my_ticket: Vec<i32> = lines[i].split(',').map(|n| n.parse().unwrap()).collect();
     // down to first ticket
     i += 3;
     let mut other_tickets: Vec<Box<Vec<i32>>> = Vec::new();
 
     while i < lines.len() {
-        other_tickets.push(Box::new(lines[i].split(',').map(|n| n.parse().unwrap()).collect()));
+        let ticket: Vec<i32> = lines[i].split(',').map(|n| n.parse().unwrap()).collect();
+        let mut is_valid_ticket = true;
+        for num in &ticket {
+            let mut is_valid_num = false;
+            for (_, rules) in &fields {
+                if is_valid_for_rule(rules, *num) {
+                    is_valid_num = true;
+                }
+            }
+            if !is_valid_num {
+                is_valid_ticket = false;
+            }
+        }
+        if is_valid_ticket {
+            other_tickets.push(Box::new(ticket));
+        }
         i += 1;
     }
 
-    let mut invalid_values = 0;
+    let mut valid_cols_for_field: HashMap<String, HashSet<usize>> = HashMap::new();
 
-    for ticket in other_tickets {
-        let b: &Vec<i32> = ticket.borrow();
-        for value in b {
-            let mut is_valid = false;
-            for (_, rules) in &fields {
-                if is_valid_for_rule(rules, *value) {
-                    is_valid = true;
+    for field in &fields {
+        for i in 0..my_ticket.len() {
+            let mut is_valid_for_col = true;
+            for ticket in &other_tickets {
+                let t: &Vec<i32> = ticket.borrow();
+                if !is_valid_for_rule(field.1, t[i]) {
+                    is_valid_for_col = false;
                 }
             }
-            if !is_valid {
-                invalid_values += *value;
+            if is_valid_for_col {
+                valid_cols_for_field.entry(field.0.to_string()).or_insert(HashSet::new()).insert(i);
             }
         }
     }
-    println!("{}", invalid_values);
+
+    let mut placed: HashMap<String, usize> = HashMap::new();
+
+    while valid_cols_for_field.len() > 0 {
+        let (field, col) = {
+            let mut as_vec: Vec<(&String, &HashSet<usize>)> = valid_cols_for_field.iter().collect();
+            as_vec.sort_by(|a, b| b.1.len().cmp(&a.1.len()));
+            let (field, colSet) = as_vec.pop().unwrap();
+            let col = *colSet.iter().next().unwrap();
+            placed.insert(field.to_string(), col);
+            (field.to_string(), col)
+        };
+        valid_cols_for_field.remove(&field);
+        for (_, set) in &mut valid_cols_for_field {
+            set.remove(&col);
+        }
+    }
+
+    let mut mult: i64 = 1;
+    for (field_name, col) in &placed {
+        if field_name.starts_with("departure") {
+            mult *= my_ticket[*col] as i64;
+        }
+    }
+    println!("{}", mult);
 }
